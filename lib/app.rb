91ec0cad366c5739a99eb0db13b881d7d059bc15
R4ja34 <raja.lalaina@gmail.com>
2023-10-15 18:10:56 +0200

    fonction générique

--- lib/app.rb
+++ lib/app.rb
@@ -1,66 +1,62 @@
 require 'nokogiri'
 require 'open-uri'
-require 'http'
 
-
-def crypto_name(page)
-  all_name = []
-  name_crypto = page.xpath('//td[contains(@class, "by__symbol")]')
-  name_crypto.each do |crypto_name|
-    name = crypto_name.text
-    all_name << name
-    end
-    return all_name
+# Fonction pour extraire des données à partir d'éléments
+def extract_data_from_elements(elements)
+  data_list = []
+  elements.each do |element|
+    data = element.text.strip
+    data_list << data unless data.empty?
   end
-
-def crypto_price(page)
-  all_price = []
-  price_crypto = page.xpath('//*[@id="__next"]/div[2]/div[2]/div/div[1]/div/div[2]/div[3]/div/table/tbody/tr/td[5]')
-  price_crypto.each do |crypto_price|
-    price = crypto_price.text.strip
-    all_price << price unless price.empty?
-    end
-    return all_price
-  end
-
-def crypto_array(names, prices)
-  crypto_hash = names.zip(prices).to_h # .zip permet de relier deux liste
-  # Créez un tableau à partir du hash
-  crypto_a = crypto_hash.map{ |crypt, price| { crypt => price } }
-  return crypto_a
+  data_list
 end
 
-def trouver_prix_crypto_dans_array(array, symbole)
-  array.each do |crypto_hash|
-    prix = crypto_hash[symbole]
-    if prix
-      puts "Le prix de #{symbole} est #{prix}"
-      return # Si trouvé, sortir de la boucle
-    end
-  end
-  puts "La cryptomonnaie #{symbole} n'a pas été trouvée."
+# Fonction pour extraire des informations à partir d'une URL et une expression XPath
+def extract_info_from_url(url, xpath_expression)
+  page = Nokogiri::HTML(URI.open(url))
+  info_elements = page.xpath(xpath_expression)
+  extract_data_from_elements(info_elements)
 end
-  page = Nokogiri::HTML(URI.open("https://coinmarketcap.com/all/views/all/"))
-  name = crypto_name(page)
-  price = crypto_price(page)
-  array = crypto_array(name, price)
 
-  puts "Choisissez une action :"
-  puts "1. tableau de tout les crypto"
-  puts "2. recherche du prix d'une crypto pas son symbole"
-  print "Entrez votre choix d'action : "
-  choix = gets.chomp.to_i
+# Fonction pour créer un tableau associatif (hash) à partir de deux listes
+def create_hash_from_lists(list1, list2)
+  list1.zip(list2).to_h
+end
 
-
-  
-  case choix
-  when 1
-    puts "Voici l'ensemble du tableau."
-    puts array
-  when 2
-    print " veuiller entrer le symbol de la crypto rechercher : "
-    symbol_of_crypto = gets.chomp
-    trouver_prix_crypto_dans_array(array, symbol_of_crypto)
+# Fonction pour rechercher le prix d'une crypto par son symbole
+def trouver_prix_crypto_dans_hash(data, symbol)
+  if data.key?(symbol)
+    puts "Le prix de #{symbol} est #{data[symbol]}"
   else
-    puts "Choix invalide. Veuillez sélectionner 1 ou 2."
+    puts "La cryptomonnaie #{symbol} n'a pas été trouvée."
   end
+end
+
+url = "https://coinmarketcap.com/all/views/all/"
+xpath_for_crypto_names = '//td[contains(@class, "by__symbol")]'
+xpath_for_crypto_prices = '//*[@id="__next"]/div[2]/div[2]/div/div[1]/div/div[2]/div[3]/div/table/tbody/tr/td[5]'
+
+# Exemple d'utilisation pour extraire des données et créer un tableau associatif
+crypto_names = extract_info_from_url(url, xpath_for_crypto_names)
+crypto_prices = extract_info_from_url(url, xpath_for_crypto_prices)
+crypto_data = create_hash_from_lists(crypto_names, crypto_prices)
+
+puts "Choisissez une action :"
+puts "1. Afficher l'ensemble du tableau des cryptomonnaies et de leurs prix"
+puts "2. Rechercher le prix d'une crypto par son symbole"
+print "Entrez votre choix d'action : "
+choix = gets.chomp.to_i
+
+case choix
+when 1
+  puts "Voici l'ensemble du tableau :"
+  crypto_data.each do |symbol, price|
+    puts "#{symbol}: #{price}"
+  end
+when 2
+  print "Veuillez entrer le symbole de la crypto recherchée : "
+  symbol_of_crypto = gets.chomp
+  trouver_prix_crypto_dans_hash(crypto_data, symbol_of_crypto)
+else
+  puts "Choix invalide. Veuillez sélectionner 1 ou 2."
+end
